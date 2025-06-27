511b0916a425af1954b52a85bd8455fc660bb6d3
tarunmalik330 <maliktarun2005@gmail.com>
2025-06-27 11:11:26 +0530

    no change

---
+++ src/components/TradeAccount.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+
+const TradeAccount = () => {
+  return (
+    <div className="pt-[160px] bg-[#070508]">
+      <div className="container xl:max-w-[1040px] mx-auto px-3">
+        <div className="flex flex-row flex-wrap">
+          <div className="w-3/12">
+            <div className="bg-[#71CED0] bg-opacity-10">
+              <div className="bg-[#70CCCE] py-[14px] px-5">
+                <p className="font-semibold text-[#292929] text-xl leading-[120%]">
+                  Account Type
+                </p>
+              </div>
+              <p className="pl-5 pt-6 pb-3 text-white text-base border-b border-b-solid border-b-[#71CED0] border-opacity-20">
+                Leverage
+              </p>
+              <p className="pl-5 py-3 text-white text-base border-b border-b-solid border-b-[#71CED0] border-opacity-20">
+                Spreads
+              </p>
+              <p className="pl-5 py-3 text-white text-base border-b border-b-solid border-b-[#71CED0] border-opacity-20">
+                Commission
+              </p>
+              <p className="pl-5 py-3 text-white text-base border-b border-b-solid border-b-[#71CED0] border-opacity-20">
+                Platform Fees
+              </p>
+              <p className="pl-5 py-3 text-white text-base border-b border-b-solid border-b-[#71CED0] border-opacity-20">
+                Bonuses
+              </p>
+              <p className="pl-5 py-3 text-white text-base border-b border-b-solid border-b-[#71CED0] border-opacity-20">
+                Account Manager
+              </p>
+              <p className="pl-5 py-3 text-white text-base border-b border-b-solid border-b-[#71CED0] border-opacity-20">
+                Mobile Trading
+              </p>
+              <p className="pl-5 py-3 text-white text-base border-b border-b-solid border-b-[#71CED0] border-opacity-20">
+                Educational Resources
+              </p>
+              <p className="pl-5 py-3 text-white text-base border-b border-b-solid border-b-[#71CED0] border-opacity-20">
+                Withdrawal Processing
+              </p>
+            </div>
+          </div>
+          <div className="w-3/12">
+            <div>
+              <div className="bg-[#70CCCE] py-[14px] px-5">
+                <p className="font-semibold text-[#292929] text-xl leading-[120%]">
+                  Standard
+                </p>
+              </div>
+              <p className="pl-5 pt-6 pb-3 text-white text-base border-b border-b-solid border-b-[#71CED0] border-opacity-20">
+                Up to 1:300
+              </p>
+              <p className="pl-5 py-3 text-white text-base border-b border-b-solid border-b-[#71CED0] border-opacity-20">
+                From 0.4 Pips
+              </p>
+              <p className="pl-5 py-3 text-white text-base border-b border-b-solid border-b-[#71CED0] border-opacity-20">
+                Commission
+              </p>
+              <p className="pl-5 py-3 text-white text-base border-b border-b-solid border-b-[#71CED0] border-opacity-20">
+                Platform Fees
+              </p>
+              <p className="pl-5 py-3 text-white text-base border-b border-b-solid border-b-[#71CED0] border-opacity-20">
+                Bonuses
+              </p>
+              <p className="pl-5 py-3 text-white text-base border-b border-b-solid border-b-[#71CED0] border-opacity-20">
+                Account Manager
+              </p>
+              <p className="pl-5 py-3 text-white text-base border-b border-b-solid border-b-[#71CED0] border-opacity-20">
+                Mobile Trading
+              </p>
+              <p className="pl-5 py-3 text-white text-base border-b border-b-solid border-b-[#71CED0] border-opacity-20">
+                Educational Resources
+              </p>
+              <p className="pl-5 py-3 text-white text-base border-b border-b-solid border-b-[#71CED0] border-opacity-20">
+                Withdrawal Processing
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TradeAccount;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
+import TradeAccount from "@/components/TradeAccount";
 import "@/styles/globals.css";
 
 export default function App() {
   return (
     <>
       <Hero />
+      <TradeAccount />
     </>
   );
 }
